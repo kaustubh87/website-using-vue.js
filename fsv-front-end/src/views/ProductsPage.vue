28ce93b9bb86ec7d0ccd64b42456c3dba3cfc1e2
Kaustubh <kaustubh@Kaustubh-Vinzanekar-Mac-Pro.local>
2020-11-22 19:03:29 -0500

    Added products, productDescriptions

--- fsv-front-end/src/views/ProductsPage.vue
+++ fsv-front-end/src/views/ProductsPage.vue
@@ -1,15 +1,39 @@
+
 <template >
-    <h1>This is the Products Page</h1>
+    <div id="page-wrap">
+        <div class="grid-wrap">
+
+                <div 
+                    v-for="product in products"
+                    class="product-item"
+                    v-bind:key="product.id"
+                    >
+                    <img v-bind:src="product.imageUrl" />
+                    <h3 class="produtc-name">{{ product.name }}</h3>
+                    <p class="product-price">${{product.price }}</p>
+                    <router-link v-bind:to="'/products/' + product.id ">
+                    <button>View Details</button>
+                    </router-link>
+                </div>
+
+                
+        </div>
+    </div>
 </template>
 
 <script>
+
+import { products }  from  '../fake-data.js';
 export default {
-    
+    name: 'ProductsPage',
+    data() {
+        return {
+            products
+        };
+    }
 }
 </script>
-    export default {
-        name: 'Products page'
-    }
+    
 
 <style scoped>
   .grid-wrap {
